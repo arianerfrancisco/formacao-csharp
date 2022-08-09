116c4c8ce4080ec34c50aba4112f83fa24c23bc2
Ariane <arianerochaf@gmail.com>
2022-08-09 14:56:08 -0300

    finalizacao

---
+++ 9-CalculaPoupanca2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 9 - Calcula Poupança 2");
+
+        double investimento = 1000;
+        double rendimento = 1.005; // rendimento de 5% ao mês
+        
+        for(int ano = 1; ano <= 5 ; ano++) // considerando um investimento a longo prazo
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= rendimento;
+             }
+
+            rendimento += 0.001;
+        }
+
+        Console.WriteLine("O rendimento do seu investimento no final de 5 anos será de: " + investimento + "reais");
+
+        Console.WriteLine("Tecle enter para fechar");
+
+        Console.ReadLine();
+
+    }
+
+}
+
---
+++ P10-EncadeandoFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 10 - Encadeando laço for");
+
+        for( int contadorLinhas=1; contadorLinhas <10; contadorLinhas++ )
+        {
+            string str = new string('*', contadorLinhas);
+            Console.WriteLine(str);
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+
+        Console.ReadLine();
+
+    }
+
+}
