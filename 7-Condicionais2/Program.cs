48abec923a7654cbaeab8e8b1687e8ff6e7f7974
Ariane <arianerochaf@gmail.com>
2022-08-09 10:44:37 -0300

    while

--- 7-Condicionais2/Program.cs
+++ 7-Condicionais2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais2");
+
+        int idadeJoao = 15;
+        int qtdePessoas = 2;
+
+        if (idadeJoao >= 18 || qtdePessoas >1)
+        {
+            Console.WriteLine("Pode entrar");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar");
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+
+        Console.ReadLine();
+
+    }
+
+}
---
+++ 8-CalculaPoupanca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 8 - Calcula Poupança");
+
+        double investimento = 1000;
+        double rendimento = 0.005; // rendimento de 0,5% ao mês
+        int mes = 1;
+        
+        while (mes <= 12)
+        {
+            investimento = investimento +  investimento * rendimento;
+            Console.WriteLine("O rendimento do seu investimento no final do mês: " + mes + " será de: " + investimento + "reais");
+            mes += 1;
+        }
+   
+
+        Console.WriteLine("Tecle enter para fechar");
+
+        Console.ReadLine();
+
+    }
+
+}
+
