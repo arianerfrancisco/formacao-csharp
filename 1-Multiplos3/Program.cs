7f304b71e30b0dce8df34559e600b3687e2f1e11
Ariane <arianerochaf@gmail.com>
2022-08-09 15:04:24 -0300

    Desafio feito

---
+++ 1-Multiplos3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Desafio multiplos de 1 a 100");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+                Console.WriteLine(i);
+                                    
+        }
+
+        Console.WriteLine("Tecle enter para fechar");
+
+        Console.ReadLine();
+
+    }
+
+}
