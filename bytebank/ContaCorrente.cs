95bcca1701b3b64d423eb24d39567c1d7ca50360
Ariane <arianerochaf@gmail.com>
2022-08-10 16:39:58 -0300

    composição de classes

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public string conta ;
+        public int numero_agencia;
+        public string nome_agencia ;
+        public double saldo;
+        public bool verificador;
+
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor || valor < 0)
+            {
+              return false;
+            } else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+        public void Depositar(double valor)
+        {
+             if(valor > 0 )
+             {
+                 saldo += valor;
+             }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                destino.saldo += valor;
+                return true;
+            }
+        }
+    }
+}
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using bytebank;
+
+/*
+ContaCorrente conta1= new ContaCorrente();
+Cliente cliente2 = new Cliente();
+cliente2.nome = "José";
+
+conta1.saldo = 2000;
+Console.WriteLine("Titular: " + cliente2.nome);
+
+ContaCorrente conta2 = new ContaCorrente();
+Cliente cliente1 = new Cliente();
+conta2.saldo = 1100;
+
+Console.WriteLine("Saldo: " + conta2.saldo);
+conta2.Sacar(20);
+Console.WriteLine("Saldo após saque: " + conta2.saldo);
+conta2.Depositar(100);
+Console.WriteLine("Saldo após deposito: " + conta2.saldo);
+
+// --------------------------------------- //
+Console.WriteLine("Saldo conta1 antes da transferência: " + conta1.saldo);
+Console.WriteLine("Saldo conta2 antes da transferência: " + conta2.saldo);
+
+bool transferencia = conta1.Transferir(50, conta2);
+
+Console.WriteLine("Transferência realizada com sucesso? " + transferencia);
+Console.WriteLine("Saldo conta1 após a transferência: " + conta1.saldo);
+Console.WriteLine("Saldo conta2 após a transferência: " + conta2.saldo);
+*/
+
+ContaCorrente conta3 = new ContaCorrente();
+Cliente cliente3 = new Cliente();
+cliente3.nome = "Ariane";
+conta3.titular = cliente3; // referenciando classe Cliente
+Console.WriteLine(conta3.titular.nome);
+
+
+
+
