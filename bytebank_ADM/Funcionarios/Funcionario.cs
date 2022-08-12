55a6a8f918ac77fff32684b4d3d295d9a605bdc6
Ariane <arianerochaf@gmail.com>
2022-08-12 14:35:22 -0300

    usando this

---
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public Funcionario(int _tipo)
+        {
+            this._tipo = _tipo;
+        }
+        public string Nome { get; set; }
+        private int _tipo;     
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+        public double getBonificacao()
+        {
+            if(_tipo == 1)
+                return Salario ;
+            else
+                return Salario *= 0.1;
+
+        }
+    }
+}
--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Boas vindas ao ByteBank ADM");
+
+Funcionario funcionario1 = new Funcionario();
+funcionario1.Nome = "João";
+funcionario1.Cpf = "545452485";
+funcionario1.Salario = 4854;
+
+Console.WriteLine(funcionario1.Salario);
