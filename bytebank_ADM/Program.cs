c42cbb2dc7551bf6447a5946f09f17b9c17b95bf
Ariane <arianerochaf@gmail.com>
2022-08-16 20:03:37 -0300

    heranca

--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitarios;
 
 Console.WriteLine("Boas vindas ao ByteBank ADM");
 
-Funcionario funcionario1 = new Funcionario(2);
+Funcionario funcionario1 = new Funcionario(1);
 funcionario1.Nome = "João";
 funcionario1.Cpf = "545452485";
 funcionario1.Salario = 4854;
 
-Console.WriteLine(funcionario1.Salario);
-Console.WriteLine(funcionario1.getBonificacao()); 
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
---
+++ bytebank_ADM/Utilitarios/GerenciadorDeBonificacao.cs
@@ -0,0 +1,24 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Utilitarios
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double totalBonificacao { get; set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.totalBonificacao += funcionario.getBonificacao();
+        }
+
+        public double getBonificacao()
+        {
+           return this.totalBonificacao = 0;
+        }
+    }
+}
