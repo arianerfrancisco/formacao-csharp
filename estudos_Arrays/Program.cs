f348e10622e7808d9c800a1f5ce6dcaff5f07a60
Ariane <arianerochaf@gmail.com>
2022-08-21 11:53:35 -0300

    updatew

--- estudos_Arrays/Program.cs
+++ estudos_Arrays/Program.cs
@@ -45,4 +45,35 @@ void TestaBuscarPalavra()
         } // foi omitido o else, pois não esperamos nenhum comportamento do programa se a palavra não for encontrada.
     }
 }
+void TestaClasseArray()
+{
+    Array amostra = new double[5]; // idem: Array amostra = Array.CreateInstance(typeof(double), 5)
+}
 
+Array amostra = Array.CreateInstance(typeof(double), 5); // idem: Array amostra = new double[5];
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+//[5,9][1,8][7,1][10][6,9]
+TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+    //[1,8][5,9][6,9][7,1][10]
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+                                                             (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+}
